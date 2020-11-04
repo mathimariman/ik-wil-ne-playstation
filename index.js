a742d646166663d1ec938e11fa1f62f659140b51
Mathias Mariman <mathias.mariman@external.toyota-europe.com>
2020-11-04 11:41:01 +0100

    - open cart automatically

--- index.js
+++ index.js
@@ -3,34 +3,31 @@ const fetch = require('node-fetch');
 const cheerio = require('cheerio');
 const notifier = require('node-notifier');
 
-const psUrl = 'https://www.coolblue.be/nl/product/865867/playstation-5-digital-edition.html';
-const selector = 'div.js-desktop-order-block > div > div.grid-section-xs--gap-4.is-hidden-until-size-m > form > div > button';
+const productId = '868090';
+const productUrl = 'https://www.coolblue.be/nl/product/' + productId;
+const cartUrl= 'https://www.coolblue.be/nl/winkelmandje?add=' + productId;
+const checkoutUrl = 'https://www.coolblue.be/nl/bestellen/overzicht';
+
+const selector = '#main-content > div.grid-section-xs--gap-4.grid-section-m--gap-5 > div > div.grid-unit-xs--col-12.grid-unit-m--col-6.grid-unit-xl--col-5.js-sticky-bar-trigger > div > div.grid-section-xs--gap-4.grid-section-m--gap-5.js-order-block > div.is-hidden-until-size-m > div.js-desktop-order-block > div.grid-section-xs--gap-4 > a > div > div.icon-with-text__text';
 
 async function run() {
         await sleep(3000);
         console.log("Scraping")
-        fetch(psUrl)
+        fetch(productUrl)
             .then(res => res.text())
             .then(body => cheerio.load(body))
             .then($ => $(selector))
-            .then(elements => elements.length > 0 ? openBrowser() : run());
+            .then(elements => elements.length === 0 ? openBrowser() : run());
     }
 
 function sleep(ms) {
     return new Promise(resolve => setTimeout(resolve, ms));
 }
 
-function openBrowser() {
-    open(psUrl);
-    notifier.notify({
-            title: 'PlayStation 5 beschikbaar',
-            message: 'HAAL DIE CREDIT CARD AL MAAR BOVEN!',
-            sound: 'Ping'
-        },
-        function (err, response) {
-            console.error(err);
-        }
-    );
+async function openBrowser() {
+    open(cartUrl);
+    await sleep(200);
+    open(checkoutUrl);
 }
 
 run();
